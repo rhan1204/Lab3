115eb43a3347569c491360b73ad582ac8335b020
rohan <rohan@ucsd.edu>
2016-04-20 10:05:04 -0700

    second

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,27 @@
+package tests;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.rohan.lab3.MainActivity;
+import com.example.rohan.lab3.R;
+
+/**
+ * Created by rohan on 4/20/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+        MainActivity mainActivity;
+
+        public JUnit_test() {
+            super(MainActivity.class);
+        }
+
+
+        public void test_first() {
+            mainActivity = getActivity();
+
+
+
+            assertEquals(4,2+2);
+        }
+    }
+
